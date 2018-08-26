1eb3c4cb9e14e3b2dea224387494e5a6c0b716a0
Enzo Teles <enzo.teles@finners.com.br>
2018-08-25 23:57:51 -0300

    criando text view e o botão ok

--- app/src/main/java/br/com/netshoes/teste/MainActivity.kt
+++ app/src/main/java/br/com/netshoes/teste/MainActivity.kt
@@ -2,11 +2,20 @@ package br.com.netshoes.teste
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tv_name.text = "Teste"
+        bt_ok.setOnClickListener {
+
+            tv_name.text = "Teste butão"
+        }
+
+
     }
 }
